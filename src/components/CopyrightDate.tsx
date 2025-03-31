f1835b10c966645b27cb0fde84f3ff2828a322ef
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-03-30 21:29:25 -0300

    Transforms a TSX component into a Astro component

--- src/components/CopyrightDate.tsx
+++
@@ -1,8 +0,0 @@
-import { currentYear } from "@lib/utils";
-
-export default function CopyrightDate() {
-
-  return (
-    <div class="text-sm mt-2">&copy; {currentYear} | All rights reserved</div>
-  );
-}
