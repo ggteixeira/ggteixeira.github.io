4da89169db73ac408efd49cc5803a65d1a602e8d
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-08-27 11:40:39 -0300

    Updates BIO and puts a dynamic yearsOfExperience counter

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,10 +3,14 @@ export default function About({
 }: {
   numberOfParagraphs?: number;
 }) {
-  const textArray = [
-    "Software Developer with experience with Python and Front-End Development. Currently interested in Back-End development with .NET and C#.",
+  const yearsOfExperience = new Date().getFullYear() - 2018;
 
-    "I am also interested in Generative Syntax, Formal Semantics, Natural Language Processing (NLP) and Analytic Philosophy, since I am also a bachelor in Linguistics.",
+  const textArray = [
+    "Software Developer with experience in Front-End Development with React (JavaScript/TypeScript), and also interested in Back-End Development with ASP.NET Core, NodeJS, and Django.",
+
+    `In the last ${yearsOfExperience} years, I’ve been working with programming languages like JavaScript, TypeScript, Python, C#, and with frameworks like ReactJS, GatsbyJS, Astro, NodeJS, Django, .NET Core, and spaCy.`,
+
+    "I am also interested in Functional Programming with Elixir, and I am experienced in NLP (Natural Language Processing), Generative Syntax and Formal Semantics.",
   ];
 
   return (
