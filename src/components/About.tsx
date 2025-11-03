9ed089102360cee2034a0c33e9808c60fda1bfc3
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2025-11-02 22:43:51 -0300

    Removes YOE and updates description

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,14 +3,12 @@ export default function About({
 }: {
   numberOfParagraphs?: number;
 }) {
-  const yearsOfExperience = new Date().getFullYear() - 2018;
-
   const textArray = [
     "Software Developer with experience in Front-End Development with React (JavaScript/TypeScript), and also interested in Back-End Development with ASP.NET Core, NodeJS, and Django.",
 
-    `In the last ${yearsOfExperience} years, I’ve been working with programming languages like JavaScript, TypeScript, Python, C#, and with frameworks like ReactJS, GatsbyJS, Astro, NodeJS, Django, .NET Core, and spaCy.`,
+    `I’ve been working with programming languages like JavaScript, TypeScript, Python, C#, and with frameworks like ReactJS, GatsbyJS, Astro, NodeJS, Django, .NET Core, and spaCy.`,
 
-    "I am also interested in Functional Programming with Elixir, and I am experienced in NLP (Natural Language Processing), Generative Syntax and Formal Semantics.",
+    "I am also interested in Generative Syntax and Formal Semantics, and I am experienced in NLP (Natural Language Processing)"
   ];
 
   return (
