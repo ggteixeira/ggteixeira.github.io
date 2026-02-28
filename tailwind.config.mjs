d95a638066c0cb2382f121248f3150447fb502a1
Gui Teixeira <24235344+ggteixeira@users.noreply.github.com>
2026-02-28 12:43:56 -0300

    updates light/dark colors to avoid pure black and white

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
-import defaultTheme from "tailwindcss/defaultTheme";
 import typography from "@tailwindcss/typography";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,6 +7,10 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        black: "#121212",
+        white: "#fafafa",
+      },
       fontFamily: {
         sans: ["Atkinson", ...defaultTheme.fontFamily.sans],
       },
