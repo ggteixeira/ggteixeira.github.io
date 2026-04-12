a5327994779dc1675fc02e14c1d4ddf2b240d9d4
Gui Teixeira <24235344+ggteixeira@users.noreply.github.com>
2026-04-12 00:25:45 -0300

    feat: add "new" and "updated" badges to ArrowCard

--- src/components/ArrowCard.tsx
+++ src/components/ArrowCard.tsx
@@ -1,11 +1,39 @@
-import { For } from "solid-js";
-import { formatDate, readingTime } from "@lib/utils";
+import { daysSince, formatDate, readingTime } from "@lib/utils";
 import type { CollectionEntry } from "astro:content";
+import { For } from "solid-js";
 
 type Props = {
   entry: CollectionEntry<"garden"> | CollectionEntry<"projects">;
   pill?: boolean;
   minimal?: boolean;
+};
+
+const ArrowIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke-width="2.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="stroke-current group-hover:stroke-black group-hover:dark:stroke-white"
+    >
+      <line
+        x1="5"
+        y1="12"
+        x2="19"
+        y2="12"
+        class="scale-x-0 group-hover:scale-x-100 translate-x-4 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
+      />
+      <polyline
+        points="12 5 19 12 12 19"
+        class="translate-x-0 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
+      />
+    </svg>
+  );
 };
 
 export default function ArrowCard(props: Props) {
@@ -23,12 +51,32 @@ export default function ArrowCard(props: Props) {
           )}
 
           {!props.minimal && (
-            <div class="text-sm uppercase">
-              {formatDate(props.entry.data.date)} -{" "}
-              {readingTime(props.entry.body ?? "")}
+            <div class="flex justify-between w-full">
+              <div class="text-sm uppercase">
+                {formatDate(props.entry.data.date)} -{" "}
+                {readingTime(props.entry.body ?? "")}
+              </div>
+
+              <div class="flex align-middle">
+                {props.entry.collection === "garden" &&
+                  props.entry.data.updatedDate &&
+                  daysSince(props.entry.data.updatedDate) <= 14 && (
+                    <div class="text-sm uppercase">
+                      updated: {formatDate(props.entry.data.updatedDate)}
+                    </div>
+                  )}
+
+                {props.entry.collection === "garden" &&
+                  daysSince(props.entry.data.date) <= 120 && (
+                    <div class="text-sm uppercase">new</div>
+                  )}
+
+                <ArrowIcon />
+              </div>
             </div>
           )}
         </div>
+
         <div
           class={`font-semibold ${!props.minimal ? "mt-3" : "mt-0"} text-black dark:text-white`}
         >
@@ -49,29 +97,6 @@ export default function ArrowCard(props: Props) {
           </ul>
         )}
       </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="stroke-current group-hover:stroke-black group-hover:dark:stroke-white"
-      >
-        <line
-          x1="5"
-          y1="12"
-          x2="19"
-          y2="12"
-          class="scale-x-0 group-hover:scale-x-100 translate-x-4 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
-        />
-        <polyline
-          points="12 5 19 12 12 19"
-          class="translate-x-0 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
-        />
-      </svg>
     </a>
   );
 }
