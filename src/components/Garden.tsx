1c68f264bdd00700aa8bd359ee5b089f4f3a7b3d
Gui Teixeira <24235344+ggteixeira@users.noreply.github.com>
2026-09-13 14:47:50 -0300

    Disclaimer

--- src/components/Garden.tsx
+++ src/components/Garden.tsx
@@ -42,51 +42,53 @@ export default function Garden(props: Props) {
 
   return (
     <div class="grid grid-cols-1 sm:grid-cols-3 gap-6">
-      <div class="col-span-3 sm:col-span-1">
-        <div class="sticky top-24">
-          <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">
-            {props.strings.filter}
-          </div>
-          <ul class="flex flex-wrap sm:flex-col gap-1.5 mb-8">
-            <For each={props.tags}>
-              {(tag) => (
-                <li>
-                  <button
-                    onClick={() => toggleTag(tag)}
-                    class={cn(
-                      "w-full px-2 py-1 rounded",
-                      "whitespace-nowrap overflow-hidden overflow-ellipsis",
-                      "flex gap-2 items-center",
-                      "bg-black/5 dark:bg-white/10",
-                      "hover:bg-black/10 hover:dark:bg-white/15",
-                      "transition-colors duration-300 ease-in-out",
-                      filter().has(tag) && "text-black dark:text-white",
-                    )}
-                  >
-                    <svg
+      {props.tags.length > 0 && (
+        <div class="col-span-3 sm:col-span-1">
+          <div class="sticky top-24">
+            <div class="text-sm font-semibold uppercase mb-2 text-black dark:text-white">
+              {props.strings.filter}
+            </div>
+            <ul class="flex flex-wrap sm:flex-col gap-1.5 mb-8">
+              <For each={props.tags}>
+                {(tag) => (
+                  <li>
+                    <button
+                      onClick={() => toggleTag(tag)}
                       class={cn(
-                        "size-5 fill-black/50 dark:fill-white/50",
+                        "w-full px-2 py-1 rounded",
+                        "whitespace-nowrap overflow-hidden overflow-ellipsis",
+                        "flex gap-2 items-center",
+                        "bg-black/5 dark:bg-white/10",
+                        "hover:bg-black/10 hover:dark:bg-white/15",
                         "transition-colors duration-300 ease-in-out",
-                        filter().has(tag) && "fill-black dark:fill-white",
+                        filter().has(tag) && "text-black dark:text-white",
                       )}
                     >
-                      <use
-                        href={`/ui.svg#square`}
-                        class={cn(!filter().has(tag) ? "block" : "hidden")}
-                      />
-                      <use
-                        href={`/ui.svg#square-check`}
-                        class={cn(filter().has(tag) ? "block" : "hidden")}
-                      />
-                    </svg>
-                    {tag}
-                  </button>
-                </li>
-              )}
-            </For>
-          </ul>
+                      <svg
+                        class={cn(
+                          "size-5 fill-black/50 dark:fill-white/50",
+                          "transition-colors duration-300 ease-in-out",
+                          filter().has(tag) && "fill-black dark:fill-white",
+                        )}
+                      >
+                        <use
+                          href={`/ui.svg#square`}
+                          class={cn(!filter().has(tag) ? "block" : "hidden")}
+                        />
+                        <use
+                          href={`/ui.svg#square-check`}
+                          class={cn(filter().has(tag) ? "block" : "hidden")}
+                        />
+                      </svg>
+                      {tag}
+                    </button>
+                  </li>
+                )}
+              </For>
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
       <div class="col-span-3 sm:col-span-2">
         <div class="flex flex-col">
           <div class="text-sm uppercase mb-2">
